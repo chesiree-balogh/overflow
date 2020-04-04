9dbd08da30ddaebb4acd641232943a5404f3c73d
Hendrixm7 <60823819+Hendrixm7@users.noreply.github.com>
2020-04-04 15:31:41 -0400

    addhero (#1)
    * addhero
    
    * remove yarn lock file
    
    * changes

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -5,6 +5,7 @@ import { Home } from './pages/Home'
 import HelloWorld from './pages/_template/HelloWorld'
 import HeyWorld from './pages/_template/HeyWorld'
 import NotFound from './pages/NotFound'
+import SubmitQuestionPage from './pages/SubmitQuestionPage'
 import SearchPage from './pages/SearchPage'
 import './custom.scss'
 export default class App extends Component {
@@ -15,6 +16,7 @@ export default class App extends Component {
       <Layout>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/submit-question" component={SubmitQuestionPage} />
           <Route exact path="/counter" component={HelloWorld} />
           <Route exact path="/typescript" component={HeyWorld} />
           <Route exact path="/search" component={SearchPage} />
